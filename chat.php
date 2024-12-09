a8c989932ac7db7844d59ee503ea12a62d967e60
Abdulloh Umar <iniautok@gmail.com>
2024-12-09 09:46:16 +0700

    users.php

--- chat.php
+++ chat.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Realtime Chat App</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-  </head>
+<?php include_once "header.php"; ?>
   <body>
     <div class="wrapper">
       <section class="chat-area">
         <header>
-          <a href="index.html" class="back-icon"
+          <a href="users.php" class="back-icon"
             ><i class="fa fa-arrow-left"></i
           ></a>
           <img src="aku2.jpg" alt="" />
--- users.php
+++ users.php
@@ -1,24 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Realtime Chat App</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
-  </head>
+<?php
+  session_start();
+  if(!isset($_SESSION['unique_id'])){
+    header("location: login.php");
+  }
+?>
+<?php include_once "header.php"; ?>
   <body>
     <div class="wrapper">
       <section class="users">
         <header>
+        <?php
+          include_once "php/config.php";
+          $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+          if(mysqli_num_rows($sql) > 0){
+            $row = mysqli_fetch_assoc($sql);
+          }
+        ?>
           <div class="content">
-            <img src="aku2.jpg" alt="" />
+            <img src="php/images/<?php echo $row['img'] ?>" alt="" />
             <div class="details">
-              <span>Ngumar</span>
-              <p>Active Now</p>
+              <span><?php echo $row['fname'] . " " . $row['lname']?></span>
+              <p> <?php echo $row['status'] ?></p>
             </div>
           </div>
           <a href="#" class="logout">Logout</a>
@@ -29,66 +31,7 @@
           <button><i class="fa fa-search"></i></button>
         </div>
         <div class="users-list">
-          <a href="#">
-            <div class="content">
-              <img src="aku2.jpg" alt="" />
-              <div class="details">
-                <span>Ngumar</span>
-                <p>This is test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fa fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="aku2.jpg" alt="" />
-              <div class="details">
-                <span>Ngumar</span>
-                <p>This is test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fa fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="aku2.jpg" alt="" />
-              <div class="details">
-                <span>Ngumar</span>
-                <p>This is test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fa fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="aku2.jpg" alt="" />
-              <div class="details">
-                <span>Ngumar</span>
-                <p>This is test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fa fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="aku2.jpg" alt="" />
-              <div class="details">
-                <span>Ngumar</span>
-                <p>This is test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fa fa-circle"></i></div>
-          </a>
-          <a href="#">
-            <div class="content">
-              <img src="aku2.jpg" alt="" />
-              <div class="details">
-                <span>Ngumar</span>
-                <p>This is test message</p>
-              </div>
-            </div>
-            <div class="status-dot"><i class="fa fa-circle"></i></div>
-          </a>
+          
         </div>
       </section>
     </div>
