1299c5e3e80468d98dbfa62a0982b2bb968520ad
Abdulloh Umar <iniautok@gmail.com>
2024-12-10 13:29:06 +0700

    finish

--- php/data.php
+++ php/data.php
@@ -1,13 +1,33 @@
 <?php
-    while ($row = mysqli_fetch_assoc($sql)) {
-        $output .= '<a href="chat.php?user_id='.$row['unique_id'].'">
+while ($row = mysqli_fetch_assoc($sql)) {
+    $sql2 = "SELECT * FROM messages WHERE (incoming_msg_id = {$row['unique_id']} 
+                OR outgoing_msg_id = {$row['unique_id']}) AND (outgoing_msg_id = {$outgoing_id} 
+                OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
+    $query2 = mysqli_query($conn, $sql2);
+    if (mysqli_num_rows($query2) > 0) {
+        $row2 = mysqli_fetch_assoc($query2);
+        $result = $row2['msg'];
+        ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+    } else {
+        $result = "No Message available";
+        $you = ""; // Tidak ada data, kosongkan variabel $you
+    }
+
+
+    (strlen($result) > 22) ? $msg = substr($result, 0, 22) . '...' : $msg = $result;
+    
+    //check user online or offline
+    ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
+
+
+    $output .= '<a href="chat.php?user_id=' . $row['unique_id'] . '">
                         <div class="content">
-                            <img src="php/images/'.$row['img'].'" alt="" />
+                            <img src="php/images/' . $row['img'] . '" alt="" />
                             <div class="details">
-                                <span>'.$row['fname'].' '.$row['lname'].'</span>
-                                <p>'.$row['status'].'</p>
+                                <span>' . $row['fname'] . ' ' . $row['lname'] . '</span>
+                                <p>' . $you . $msg . '</p>
                             </div>
                         </div>
-                        <div class="status-dot"><i class="fa fa-circle"></i></div>
+                        <div class="status-dot '. $offline .'"><i class="fa fa-circle"></i></div>
                     </a>';
-    }
+}
